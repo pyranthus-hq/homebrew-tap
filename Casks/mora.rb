4131308f1a4c91bd37b612fbde7d43bc27a8ec47
Adit Karode <adit.karode@gmail.com>
2026-06-03 19:59:08 -0400

    mora 0.3.0

--- Casks/mora.rb
+++ Casks/mora.rb
@@ -1,13 +1,13 @@
 cask "mora" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.2.0"
+  version "0.3.0"
 
   on_arm do
-    sha256 "9bf6fec0507d8d056b23d7744ff2433194f542521cac61dcdb45c8ab17000e5d"
+    sha256 "c397c7936b56a59242e002ed4416e0ecb255fc64b98f694c3b1f16929d880eec"
   end
   on_intel do
-    sha256 "5c9555d3c13c964ed9c80f14bd13142e17a50b5f1ef5c7ac7753e990483a01cf"
+    sha256 "e4751ccfc2a014ab101931445012bf3c019643bbc499816d17cffa3f16ac51b4"
   end
 
   url "https://github.com/pyranthus-hq/mora/releases/download/v#{version}/mora_#{version}_darwin_#{arch}.tar.gz"
